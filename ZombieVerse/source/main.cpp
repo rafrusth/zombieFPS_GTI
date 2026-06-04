f4f4584d64d106b9642a9ac66f2206f0afbfb84d
rafrusth <rafifsetya41@gmail.com>
2026-06-05 01:19:51 +0700

    fix: modularisasi final sekali banget pls yeah

--- ZombieVerse/source/main.cpp
+++ ZombieVerse/source/main.cpp
@@ -95,10 +95,10 @@ void display() {
     setMaterial(1.0f,1.0f,1.0f);
     glBegin(GL_QUADS);
         glNormal3f(0,1,0);
-        glTexCoord2f( 0.0f, 0.0f); glVertex3f(-100,-1,-100);
-        glTexCoord2f( 0.0f,50.0f); glVertex3f(-100,-1, 100);
-        glTexCoord2f(50.0f,50.0f); glVertex3f( 100,-1, 100);
-        glTexCoord2f(50.0f, 0.0f); glVertex3f( 100,-1,-100);
+        glTexCoord2f(0.0f,0.0f); glVertex3f(-100,-1,-100);
+        glTexCoord2f(0.0f,50.0f); glVertex3f(-100,-1,100);
+        glTexCoord2f(50.0f,50.0f); glVertex3f(100,-1,100);
+        glTexCoord2f(50.0f,0.0f); glVertex3f(100,-1,-100);
     glEnd();
     glDisable(GL_TEXTURE_2D);
 
